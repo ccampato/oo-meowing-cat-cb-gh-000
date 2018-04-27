ba2176f79e7bc2d6655e3bfca751fc68ed5a6241
Caio Sanches <ccampato@gmail.com>
2018-04-27 02:28:14 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,7 +1,9 @@
-## code your solution here. 
+## code your solution here.
 class Cat
-  attr_accessor: name
-
+  def name(name)
+    attr_accessor: name
+  end
+  
   def meow
     puts "meow!"
   end
